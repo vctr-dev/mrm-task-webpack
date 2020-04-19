41698abd1790916d1f35891243815d6f187a9700
Victor Chan <victor.z.chan@gmail.com>
2020-04-19 14:53:49 +1000

    Made task perform installation and config
    Install dependencies
    Write package.json scripts
    Create files

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
-function task() {}
+const { packageJson, install, copyFiles, makeDirs } = require("mrm-core");
+const fs = require("fs");
+const path = require("path");
 
-module.exports = task;
+module.exports = webpackTask;
+
+function webpackTask() {
+	installDependencies();
+	writeFiles();
+	writePackageJsonScripts();
+}
+
+function installDependencies() {
+	const webpackPackages = ["webpack", "webpack-cli", "webpack-dev-server"];
+	const loaderPackages = ["babel-loader", "style-loader", "css-loader"];
+	const loaderDependencies = ["@babel/core", "@babel/preset-env"];
+	install([...webpackPackages, ...loaderPackages, ...loaderDependencies], {
+		yarn: true,
+	});
+}
+
+function writeFiles() {
+	copyFiles(path.join(__dirname, "templates"), "webpack.config.js");
+	copyFiles(path.join(__dirname, "templates"), "index.html");
+	makeDirs("src");
+	const indexJsPath = path.join("src", "index.js");
+	if (!fs.existsSync(indexJsPath)) {
+		fs.writeFileSync(indexJsPath, "");
+		console.info(`Create ${indexJsPath}`);
+	}
+}
+
+function writePackageJsonScripts() {
+	packageJson()
+		.setScript("start:dev", "webpack-dev-server")
+		.setScript("build:dev", "webpack")
+		.setScript("build", "webpack --mode production")
+		.save();
+}
---
+++ templates/webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+	module: {
+		rules: [
+			{
+				test: /\.m?js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env"],
+					},
+				},
+			},
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"],
+			},
+		],
+	},
+};
